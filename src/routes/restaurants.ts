2e8893b105cfbf8ad05c99d2c2cf2ecc55d9fb18
aljaziz <turjatheharbingerofdeath@gmail.com>
2025-10-26 17:02:03 +0600

    redis configuration added

--- src/routes/restaurants.ts
+++ src/routes/restaurants.ts
@@ -1,5 +1,14 @@
+import { validate } from "@/middlewares/validate";
+import { Restaurant, RestaurantSchema } from "@/Schema/restaurant";
+import { initializedRedisClient } from "@/utils/client";
 import { Router } from "express";
 
 const router = Router();
+
+router.post("/", validate(RestaurantSchema), async (req, res) => {
+    const data = req.body as Restaurant;
+    const client = await initializedRedisClient();
+    res.send(data);
+});
 
 export default router;
---
+++ src/utils/client.ts
@@ -0,0 +1,17 @@
+import { createClient, type RedisClientType } from "redis";
+
+let client: RedisClientType | null = null;
+
+export const initializedRedisClient = async () => {
+    if (!client) {
+        client = createClient();
+        client.on("error", (error) => {
+            console.error(error);
+        });
+        client.on("connect", () => {
+            console.log("Redis Connected");
+        });
+        await client.connect();
+    }
+    return client;
+};
